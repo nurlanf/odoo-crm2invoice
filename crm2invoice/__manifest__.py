980f17be193a34e5f749f47afdf90b797a269dc1
nurlanf <nurlan.farajov@gmail.com>
2018-10-09 22:25:43 +0200

    update

--- crm2invoice/__manifest__.py
+++ crm2invoice/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account_invoice','crm'],
+    'depends': ['base','crm','account'],
 
     # always loaded
     'data': [
