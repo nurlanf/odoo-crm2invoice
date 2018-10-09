71ca8441793514ce68e1aad0ed1ce0dd2b38b95a
nurlanf <nurlan.farajov@gmail.com>
2018-10-09 23:31:22 +0200

    update

--- crm2invoice/models/models.py
+++ crm2invoice/models/models.py
@@ -18,3 +18,15 @@ class Account(models.Model):
     _inherit = 'account.invoice'
 
     crm_lead = fields.Many2one('crm.lead', string="Lead/Opportunity", readonly="True")
+
+class CRM(models.Model):
+    _inherit = 'crm.lead'
+
+    x_crm_lead__account_invoice_count = fields.Integer(compute ='_results')
+
+    @api.multi
+    def _results(self):
+    results = self.env['account.invoice'].read_group([('crm_lead', 'in', self.ids)], 'crm_lead', 'crm_lead')
+    dic = {}
+    for x in results: dic[x['crm_lead'][0]] = x['crm_lead_count']
+    for record in self: record['x_crm_lead__account_invoice_count'] = dic.get(record.id, 0)
